043f61a36e3930a592392dc2b069483d7eb6c290
ezequiel-creador <ekielderivia@gmail.com>
2026-06-08 16:42:10 -0300

    File yml Azure Pipelines

--- src/main/java/com/proyect/testing/security/ApiSecurityConfig.java
+++ src/main/java/com/proyect/testing/security/ApiSecurityConfig.java
@@ -3,7 +3,6 @@ package com.proyect.testing.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
